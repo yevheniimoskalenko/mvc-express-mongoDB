d08c3cb877b69f63e9a0565230476cfb7d9ecba6
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-01-17 22:40:34 +0200

    verify finall

---
+++ src/controllers/verify.controller.js
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+export default async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization.replace('Bearer ', '');
+  if (token === '') {
+    return res.json({
+      status: 'error',
+      message: 'не встановлений токен',
+    });
+  }
+  try {
+    const obj = jwt.verify(token, process.env.secret);
+    return res.json({
+      status: 'ok',
+      payload: {
+        obj,
+      },
+    });
+  } catch (err) {
+    return res.json(err);
+  }
+};
--- src/routes/verify.routes.js
+++ src/routes/verify.routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-
+import verify from '../controllers/verify.controller.js';
 const router = Router();
+
+router.use('/verefy', verify);
 
 export default router;
